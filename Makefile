c625daf551f06438098e1604e3cb44b95387d04d
Tatiana Kudashova <kudashova93@gmail.com>
2018-06-04 13:57:59 +0400

    help in gendiff

--- Makefile
+++ Makefile
@@ -1,7 +1,5 @@
 install: ;@echo "Installing ${PROJECT}....."; \
 	npm install
-gendiff:
-	npm run babel-node -- src/bin/gendiff.js
 publish:
 	npm publish
 lint:
--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,2 +1,9 @@
 #!/usr/bin/env node
-console.log('its ok');
+const program = require('commander');
+
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .usage(' [options] <firstConfig> <secondConfig>', '?')
+  .option('-f, --format [type]', 'Output format')
+  .parse(process.argv);
