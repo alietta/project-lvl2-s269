21260429b3745f8e59a85b0e6a84adab68a6b518
Tatiana Kudashova <kudashova93@gmail.com>
2018-06-05 15:40:42 +0400

    fix task 2

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,7 +3,7 @@ import program from 'commander';
 import genDiff from '..';
 
 program
-  .version('0.0.1')
+  .version('0.0.3')
   .description('Compares two configuration files and shows a difference.')
   .usage(' [options] <firstConfig> <secondConfig>', '?')
   .option('-f, --format [type]', 'Output format')
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import fs from 'fs';
 import _ from 'lodash';
 
+const nodeToString = {
+  stable: ({ key, before }) => `\n   ${key}: ${before}`,
+  update: ({ key, before, after }) => `\n + ${key}: ${after}\n - ${key}: ${before}`,
+  delete: ({ key, before }) => `\n - ${key}: ${before}`,
+  insert: ({ key, after }) => `\n + ${key}: ${after}`,
+};
+
 export default (beforePath, afterPath) => {
   const before = JSON.parse(fs.readFileSync(beforePath, 'utf8'));
   const after = JSON.parse(fs.readFileSync(afterPath, 'utf8'));
@@ -8,21 +15,20 @@ export default (beforePath, afterPath) => {
   const keysAfter = _.keys(after);
   const keys = keysBefore.concat(_.difference(keysAfter, keysBefore));
   const result = keys.reduce((acc, key) => {
-    let newAcc = {};
-    if (!_.has(before, key)) {
-      newAcc = { ...acc, [`+ ${key}`]: after[key] };
-    } else if (!_.has(after, key)) {
-      newAcc = { ...acc, [`- ${key}`]: before[key] };
-    } else if (before[key] === after[key]) {
-      newAcc = { ...acc, [`  ${key}`]: before[key] };
-    } else {
-      newAcc = {
-        ...acc,
-        [`+ ${key}`]: after[key],
-        [`- ${key}`]: before[key],
-      };
+    if (_.has(before, key)) {
+      if (_.has(after, key)) {
+        if (before[key] === after[key]) {
+          return [...acc, { type: 'stable', diff: { key, before: before[key] } }];
+        }
+        return [...acc,
+          {
+            type: 'update', diff: { key, after: after[key], before: before[key] },
+          }];
+      }
+      return [...acc, { type: 'delete', diff: { key, before: before[key] } }];
     }
-    return newAcc;
-  }, {});
-  return `${JSON.stringify(result, null, ' ').replace(/"|,/g, '')}\n`;
+    return [...acc, { type: 'insert', diff: { key, after: after[key] } }];
+  }, []);
+  const stringResult = result.reduce((acc, r) => `${acc}${nodeToString[r.type](r.diff)}`, '');
+  return `{${stringResult}\n}\n`;
 };
