eb6c6f4089dcb7055cc4260f2b860384bc104113
Tatiana Kudashova <kudashova93@gmail.com>
2018-06-06 13:20:55 +0400

    fix 2 task

--- src/index.js
+++ src/index.js
@@ -1,37 +1,36 @@
 import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
-import parse from './parseFunction';
+import parse from './parser';
 
 const nodeToString = {
-  unchanged: ({ key, before }) => `\n   ${key}: ${before}`,
-  updated: ({ key, before, after }) => `\n + ${key}: ${after}\n - ${key}: ${before}`,
-  deleted: ({ key, before }) => `\n - ${key}: ${before}`,
-  inserted: ({ key, after }) => `\n + ${key}: ${after}`,
+  unchanged: ({ key, before }) => `   ${key}: ${before}`,
+  updated: ({ key, before, after }) => [` + ${key}: ${after}`, ` - ${key}: ${before}`],
+  deleted: ({ key, before }) => ` - ${key}: ${before}`,
+  inserted: ({ key, after }) => ` + ${key}: ${after}`,
 };
 const parseFile = filePath => parse(path.extname(filePath))(fs.readFileSync(filePath, 'utf8'));
 
 export default (beforePath, afterPath) => {
   const before = parseFile(beforePath);
   const after = parseFile(afterPath);
-  const keysBefore = _.keys(before);
-  const keysAfter = _.keys(after);
-  const keys = keysBefore.concat(_.difference(keysAfter, keysBefore));
-  const result = keys.reduce((acc, key) => {
+  const keys = _.union(_.keys(before), _.keys(after));
+
+  const result = keys.map((key) => {
     if (_.has(before, key)) {
       if (_.has(after, key)) {
         if (before[key] === after[key]) {
-          return [...acc, { type: 'unchanged', diff: { key, before: before[key] } }];
+          return { type: 'unchanged', diff: { key, before: before[key] } };
         }
-        return [...acc,
-          {
-            type: 'updated', diff: { key, after: after[key], before: before[key] },
-          }];
+        return {
+          type: 'updated', diff: { key, after: after[key], before: before[key] },
+        };
       }
-      return [...acc, { type: 'deleted', diff: { key, before: before[key] } }];
+      return { type: 'deleted', diff: { key, before: before[key] } };
     }
-    return [...acc, { type: 'inserted', diff: { key, after: after[key] } }];
+    return { type: 'inserted', diff: { key, after: after[key] } };
   }, []);
-  const stringResult = result.reduce((acc, r) => `${acc}${nodeToString[r.type](r.diff)}`, '');
-  return `{${stringResult}\n}\n`;
+
+  const stringResult = _.flatten(result.map(r => nodeToString[r.type](r.diff)));
+  return ['{', ...stringResult, '}\n'].join('\n');
 };
---
+++ src/parser.js
@@ -0,0 +1,16 @@
+import ini from 'ini';
+import { safeLoad } from 'js-yaml';
+
+const parsers = {
+  '.json': JSON.parse,
+  '.yaml': safeLoad,
+  '.ini': ini.parse,
+};
+
+export default format => (data) => {
+  const parse = parsers[format];
+  if (!parse) {
+    throw new Error(`unkown format: ${format}`);
+  }
+  return parse(data);
+};
